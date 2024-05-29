3928064adc72f80bdab8205263b562e930464029
Felpezs <felipe.edfreire@gmail.com>
2024-05-29 11:00:47 -0300

    feat: adding validation structure to inputs

--- src/components/numberInput/NumberInput.jsx
+++ src/components/numberInput/NumberInput.jsx
@@ -1,18 +1,39 @@
 import PropTypes from "prop-types";
 import styles from "./NumberInput.module.css";
 
-const NumberInput = ({ label, name, placeholder, step = 1, min = 0 }) => {
+const NumberInput = ({
+  label,
+  name,
+  placeholder,
+  step = 1,
+  min = 0,
+  isValid = undefined,
+  handleBlur,
+  errorMsg,
+}) => {
+  let validStyle = "";
+
+  if (typeof isValid === "boolean") {
+    validStyle = isValid ? styles["valid"] : styles["invalid"];
+  }
+
   return (
     <div className={styles.input}>
-      <label htmlFor={name}>{label} </label>
+      <label htmlFor={name} className={styles["input-label"]}>
+        {label}{" "}
+      </label>
       <input
         id={name}
-        className={styles["input-field"]}
+        className={`${styles["input-field"]} ${validStyle}`}
         type="number"
         min={min}
         step={step}
         placeholder={placeholder}
+        onBlur={handleBlur}
       />
+      {errorMsg && typeof isValid === "boolean" && !isValid && (
+        <span className={styles["input-error"]}>{errorMsg}</span>
+      )}
     </div>
   );
 };
@@ -23,6 +44,9 @@ NumberInput.propTypes = {
   placeholder: PropTypes.string,
   step: PropTypes.number,
   min: PropTypes.number,
+  isValid: PropTypes.bool,
+  handleBlur: PropTypes.func,
+  errorMsg: PropTypes.string,
 };
 
 export default NumberInput;

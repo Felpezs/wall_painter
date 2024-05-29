1880633f6e4ec66f7dc78f0323552607d9311571
Felpezs <felipe.edfreire@gmail.com>
2024-05-29 11:39:22 -0300

    feat: constants for sizes

---
+++ src/constants/sizeRules.js
@@ -0,0 +1,15 @@
+export const SIZES = {
+  DOOR: {
+    HEIGHT: 1.9, //1.9m
+    WIDTH: 0.8, //0.8m
+  },
+  WINDOW: {
+    WIDTH: 2, //2m
+    HEIGHT: 1.2, //1.2m
+  },
+  CAN: [0.5, 2.5, 3.6, 18], //In liters
+  WALL_AREA: {
+    MIN: 1, //1m2
+    MAX: 50, //50m2
+  },
+};
